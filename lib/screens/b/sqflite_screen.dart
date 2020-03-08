51d5928f2b79fd869c0dcc959147dbb3db383719
chanyoung <chch_@naver.com>
2020-03-09 07:29:47 +0900

    modified sqflite_screen.dart

--- lib/screens/b/sqflite_screen.dart
+++ lib/screens/b/sqflite_screen.dart
@@ -10,23 +10,33 @@ class SqfliteScreen extends StatefulWidget {
 
 class _SqfliteScreenState extends State<SqfliteScreen> {
   /*  Member(String no, String id, String name) */
-  Database db;
-  String memberData = '초기값';
+  Database _db;
+  _RawQuery _rawQuery;
+  String _memberData = '초기값';
+  TextEditingController idController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
 
   @override
   void initState() {
-    _DB.open().then((database) => {db = database});
+    super.initState();
+    initQuery();
   }
 
   @override
   void dispose() {
-    db.close();
+    super.dispose();
+    _db.close();
   }
 
-  void _setMemberDataToRawResult() async {
-    List<Map> _result = await _selectRawQuery();
+  void initQuery() async {
+    _db = await _DB.getConnection();
+    _rawQuery = _RawQuery(database: _db);
+  }
+
+  void _showMemberData() async {
+    List<Map> _result = await _rawQuery._select();
     setState(() {
-      memberData = _result.toString();
+      _memberData = _result.toString();
     });
   }
 
@@ -37,107 +47,81 @@ class _SqfliteScreenState extends State<SqfliteScreen> {
         title: Text('DB 테스트'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: <Widget>[
-            MemberInfoForm(),
+            SizedBox(height: 50),
+            memberForm(),
             SizedBox(height: 30),
-            _flatButton(title: 'excute insert', function: _executeInsert),
+            rawQueryButtons(),
             Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                _flatButton(
-                    title: 'select', function: _setMemberDataToRawResult),
-                _flatButton(title: 'insert', function: _rawInsert),
-                _flatButton(title: 'update', function: _rawUpdate),
-                _flatButton(title: 'delete', function: _rawDelete),
-              ],
+            Container(
+              alignment: Alignment.center,
+              child: Text('--> member data: $_memberData'),
             ),
-            Divider(),
-            Text('--> member data: $memberData'),
           ],
         ),
       ),
     );
   }
 
-  ///   execute: Execute an SQL query with "no return value"
-  ///   rawInsert: Returns the "last inserted record id"
-  void _executeInsert() {
-    db.execute(
-      'insert into member(id, name) values(?, ?)',
-      ['excute', 'insert Excute'],
-    );
-  }
+  Widget memberForm() => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 80),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: idController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.alternate_email),
+                hintText: 'someone@email.com',
+                labelText: 'Id *',
+              ),
+            ),
+            TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.person_outline),
+                hintText: 'What do people call you?',
+                labelText: 'Name',
+              ),
+            )
+          ],
+        ),
+      );
 
-  /*       --- Raw SQL queries ---       */
-  void _rawInsert() async {
-    int _no = await db.rawInsert(
-      'insert into member(no, id, name) values(null, ?, ?)',
-      ['raw3', 'rawInsert'],
-    );
-    debugPrint('rawInsert no: $_no');
-  }
-
-  Future<List<Map>> _selectRawQuery() async {
-    List<Map> _result = await db.rawQuery('select * from member');
-    debugPrint('select rawQuery result: $_result');
-    return _result;
-  }
-
-  void _rawUpdate() async {
-    int _count = await db.rawUpdate(
-      'update member set id = ?, name = ? where id = ?',
-      ['첫번째 멤버', '첫번째 업데이트', '첫번째'],
-    );
-    debugPrint('rawUpdate count: $_count');
-  }
-
-  void _rawDelete() async {
-    int _count = await db.rawDelete(
-      'delete from member where id = ?',
-      ['raw2'],
-    );
-    debugPrint('rawDelete count: $_count');
-  }
-
-  /*        --- Other ---       */
-  void _transaction() async {}
+  Widget rawQueryButtons() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _flatButton(title: 'select', function: _showMemberData),
+          _flatButton(title: 'insert', function: _rawQuery?._insert),
+          _flatButton(title: 'update', function: _rawQuery?._update),
+          _flatButton(title: 'delete', function: _rawQuery?._delete),
+        ],
+      );
 
   Widget _flatButton({@required String title, @required Function function}) =>
       FlatButton(
-        color: Colors.cyan,
+        color: Colors.cyan[300],
         onPressed: () => function(),
-        child: Text(title),
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       );
 }
 
-class MemberInfoForm extends StatefulWidget {
-  @override
-  _MemberInfoFormState createState() => _MemberInfoFormState();
-}
-
-class _MemberInfoFormState extends State<MemberInfoForm> {
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        icon: Icon(Icons.person),
-        hintText: 'What do people call you?',
-        labelText: 'Name *',
-      ),
-      onSaved: (String value) {
-        // This optional block of code can be used to run
-        // code when the user saves the form.
-      },
-    );
-  }
-}
-
 class _DB {
+  static Future<Database> getConnection() async {
+    Database _database;
+    _database = await _open();
+    print('>>> ' + _database.toString());
+
+    return _database;
+  }
+
   // ## open a connection to the database
-  static Future<Database> open() async {
+  static Future<Database> _open() async {
     debugPrint('--> open database');
     //  1. database file path 설정 -> path: join(), sqlflite: getDatabasesPath()
     String path = join(await getDatabasesPath(), 'demo.db');
@@ -148,9 +132,52 @@ class _DB {
       onCreate: (db, version) {
         print('---> # onCreate database');
         return db.execute(
-          'create table member(no integer primary key autoincrement, id text not null unique, name text)',
+          '''create table member(
+                no integer primary key autoincrement
+                ,id text not null unique
+                ,name text
+            )''',
         );
       },
     );
+  }
+}
+
+class _RawQuery {
+  Database database;
+  _RawQuery({@required this.database});
+
+  ///   execute: Execute an SQL query with "no return value"
+  ///   rawInsert: Returns the "last inserted record id"
+  void _insert({String id, String name}) async {
+    int _no = await database.rawInsert(
+      'insert into member(no, id, name) values(null, ?, ?)',
+      [id, name],
+    );
+    debugPrint('rawInsert no: $_no');
+  }
+
+  Future<List<Map>> _select() async {
+    List<Map> _result = await database.rawQuery(
+      'select * from member',
+    );
+    debugPrint('select rawQuery result: $_result');
+    return _result;
+  }
+
+  void _update() async {
+    int _count = await database.rawUpdate(
+      'update member set id = ?, name = ? where id = ?',
+      ['첫번째 멤버', '첫번째 업데이트', '첫번째'],
+    );
+    debugPrint('rawUpdate count: $_count');
+  }
+
+  void _delete() async {
+    int _count = await database.rawDelete(
+      'delete from member where id = ?',
+      ['raw2'],
+    );
+    debugPrint('rawDelete count: $_count');
   }
 }
